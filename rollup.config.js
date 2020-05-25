8005b2c9925207b497b7c1207ac14e4eeb0d21d2
Martin Rafael Gonzalez <tin@devtin.io>
2020-05-25 13:54:13 -0400

    chore: add boilerplate

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import { name, version, author, license } from './package.json'
+
+const initialYear = 2020
+const yearsActive = new Date().getFullYear() !== initialYear ? `${ initialYear }-${ new Date().getFullYear() }` : initialYear
+
+const banner = `/*!
+ * ${ name } v${ version }
+ * (c) ${ yearsActive } ${ author }
+ * ${ license }
+ */`
+
+/*const plugins = [commonjs()]*/
+
+export default [
+  {
+    input: 'src/rollup-plugin-ava-test-example.js',
+    output: [
+      {
+        file: `dist/rollup-plugin-ava-test-example.js`,
+        format: 'umd',
+        banner,
+        name: 'ava2json'
+      },
+    ]
+  }
+]
